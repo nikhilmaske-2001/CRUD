093c6b78378b2042e8b446faca1304088b4d2088
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-09-01 13:37:46 +0530

    Create delete request.

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,16 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [];
+let users = [];
 
+// Get the json object of all users
 router.get('/', (req, res) => {
     console.log(users);
 
     res.send(users);
 });
 
+// Add a new user to the users object database
 router.post('/', (req, res) => {
     const user = req.body;
 
@@ -19,5 +21,23 @@ router.post('/', (req, res) => {
     res.send(`User with the new name ${user.firstName} added to the database`);
 
 });
+
+// Search for the user using unique id
+router.get('/:id', (req, res) => {
+   const {id} = req.params;
+   
+   const foundUser = users.find((user) => user.id === id);
+
+   res.send(foundUser);
+});
+
+// Delete the user using it's id
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+
+    users = users.filter((user) => user.id !== id);
+
+    res.send(`User with id ${id} is deleted`);
+})
 
 export default router;

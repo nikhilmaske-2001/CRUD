8c38203acfb569726ce90aa6ede2372680927584
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-09-01 14:01:58 +0530

    Refractor all requests in controller.

--- routes/users.js
+++ routes/users.js
@@ -1,64 +1,21 @@
 import express from "express";
-import { v4 as uuidv4 } from 'uuid';
+import { addUser, deleteUser, editUser, getUserByID, getUsers } from "../controllers/users.js";
 
 const router = express.Router();
 
-let users = [];
-
 // Get the json object of all users
-router.get('/', (req, res) => {
-    console.log(users);
-
-    res.send(users);
-});
+router.get('/', getUsers);
 
 // Add a new user to the users object database
-router.post('/', (req, res) => {
-    const user = req.body;
-
-    users.push({...user, id: uuidv4()});
-
-    res.send(`User with the new name ${user.firstName} added to the database`);
-
-});
+router.post('/', addUser);
 
 // Search for the user using unique id
-router.get('/:id', (req, res) => {
-   const {id} = req.params;
-   
-   const foundUser = users.find((user) => user.id === id);
-
-   res.send(foundUser);
-});
+router.get('/:id', getUserByID);
 
 // Delete the user using it's id
-router.delete('/:id', (req, res) => {
-    const {id} = req.params;
-
-    users = users.filter((user) => user.id !== id);
-
-    res.send(`User with id ${id} is deleted`);
-});
+router.delete('/:id',deleteUser);
 
 // Change the user details (Partially)
-router.patch('/:id', (req, res) => {
-    const {id} = req.params;
-    const {firstName, lastName, age} = req.body;
-    const user = users.find((user) => user.id === id);
-
-    if(firstName) {
-        user.firstName = firstName;
-    }
-
-    if(lastName) {
-        user.lastName = lastname;
-    }
-
-    if(age) {
-        user.age = age;
-    }
-
-    res.send(users);
-});
+router.patch('/:id', editUser);
 
 export default router;
